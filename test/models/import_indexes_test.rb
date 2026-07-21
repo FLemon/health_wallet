e5d48fc1a338551b1220fdbaa3d33a26d6704f06
FLemon <jinfengxie1986@gmail.com>
2026-07-22 00:41:41 +0100

    Create Mongoid indexes during bootstrap

--- test/models/import_indexes_test.rb
+++ test/models/import_indexes_test.rb
@@ -1,15 +1,28 @@
 require "test_helper"
+require "rake"
 
 class ImportIndexesTest < ActiveSupport::TestCase
-  test "patients have a unique identity index" do
-    index = Patient.index_specifications.find { |specification| specification.options[:unique] }
-
-    assert_equal({ name: 1, dob: 1, sex_at_birth: 1 }, index.key)
+  setup do
+    Rails.application.load_tasks unless Rake::Task.task_defined?("db:mongoid:create_indexes")
+    Rake::Task["db:mongoid:create_indexes"].reenable
+    Rake::Task["db:mongoid:create_indexes"].invoke
   end
 
-  test "assessments have a unique reference within a patient" do
-    index = Assessment.index_specifications.find { |specification| specification.options[:unique] }
+  test "patients have a physical unique identity index" do
+    index = unique_index_for(Patient, "name" => 1, "dob" => 1, "sex_at_birth" => 1)
 
-    assert_equal({ patient_id: 1, reference: 1 }, index.key)
+    assert_equal true, index["unique"]
+  end
+
+  test "assessments have a physical unique reference index within a patient" do
+    index = unique_index_for(Assessment, "patient_id" => 1, "reference" => 1)
+
+    assert_equal true, index["unique"]
+  end
+
+  private
+
+  def unique_index_for(model, key)
+    model.collection.indexes.to_a.find { |index| index["key"] == key }
   end
 end
