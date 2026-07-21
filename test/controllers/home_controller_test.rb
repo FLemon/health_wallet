953450b8d26b28a295f732a40206b992b40d8bc9
FLemon <jinfengxie1986@gmail.com>
2026-07-21 23:46:16 +0100

    Add fixed application navigation header

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,4 +10,13 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_match "Health Wallet", response.body
   end
+
+  test "displays the fixed navigation links" do
+    get root_url
+
+    assert_select "header.site-header"
+    assert_select "nav[aria-label='Primary navigation'] a[href='#{patients_path}']", text: "Patients"
+    assert_select "nav[aria-label='Primary navigation'] a[href='#{laboratory_imports_path}']", text: "Uploads"
+    assert_select "nav[aria-label='Primary navigation'] a[href='#{new_laboratory_import_path}']", text: "New upload"
+  end
 end
